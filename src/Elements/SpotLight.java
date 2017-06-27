1f280160b79a2cf2437fa79ba2f9d481763c8bc4
shalom bloch <shbloch@g.jct.ac.il>
2017-06-27 19:37:09 +0300

    ממשק משתמש

--- src/Elements/SpotLight.java
+++ src/Elements/SpotLight.java
@@ -34,7 +34,7 @@ public class SpotLight extends PointLight {
      * PARAMETERS Point3D point
      * RETURN VALUE
      *		new color every class have to add her change in light.
-     **************************************************/
+     *************************************************/
     @Override
     public Color getIntensity(Point3D point) {
         Point3D po = new Point3D(point);
